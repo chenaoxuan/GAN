9ea5434a5add8e20da5deabb9b2d8c9c52550e99
chenaoxuan <cax1165@163.com>
2022-06-27 00:46:55 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,168 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from PIL import Image
+from torch.autograd import Variable
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torchvision.utils import make_grid
+from tqdm.notebook import tqdm
+
+num_epochs = 10
+n_critic = 5
+display_step = 300
+
+
+class FashionMNIST(Dataset):
+    def __init__(self, transform=None):
+        self.transform = transform
+        fashion_df = pd.read_csv('./data/'
+                                 'fashion-mnist_train.csv')
+        print(fashion_df.shape)
+        self.labels = fashion_df.label.values
+        self.images = fashion_df.iloc[:, 1:].values.astype('uint8').reshape(-1, 28, 28)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        label = self.labels[idx]
+        img = Image.fromarray(self.images[idx])
+        if self.transform:
+            img = self.transform(img)
+        return img, label
+
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=(0.5,), std=(0.5,))
+])
+dataset = FashionMNIST(transform=transform)
+data_loader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True)
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.label_emb = nn.Embedding(10, 10)
+        self.model = nn.Sequential(
+            nn.Linear(794, 1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(1024, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x, labels):
+        x = x.view(x.size(0), 784)
+        c = self.label_emb(labels)
+        x = torch.cat([x, c], 1)
+        out = self.model(x)
+        return out.squeeze()
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.label_emb = nn.Embedding(10, 10)
+        self.model = nn.Sequential(
+            nn.Linear(110, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(1024, 784),
+            nn.Tanh()
+        )
+
+    def forward(self, z, labels):
+        z = z.view(z.size(0), 100)
+        c = self.label_emb(labels)
+        x = torch.cat([z, c], 1)
+        out = self.model(x)
+        return out.view(x.size(0), 28, 28)
+
+
+generator = Generator().cuda()
+discriminator = Discriminator().cuda()
+
+criterion = nn.BCELoss()
+
+
+def generator_train_step(batch_size, discriminator, generator, g_optimizer, criterion):
+    g_optimizer.zero_grad()
+    z = Variable(torch.randn(batch_size, 100)).cuda()
+    fake_labels = Variable(torch.LongTensor(np.random.randint(0, 10, batch_size))).cuda()
+    fake_images = generator(z, fake_labels)
+    validity = discriminator(fake_images, fake_labels)
+    g_loss = criterion(validity, Variable(torch.ones(batch_size)).cuda())
+    g_loss.backward()
+    g_optimizer.step()
+    return g_loss.item()
+
+
+def discriminator_train_step(batch_size, discriminator, generator, d_optimizer, criterion, real_images, labels):
+    d_optimizer.zero_grad()
+
+    # train with real images
+    real_validity = discriminator(real_images, labels)
+    real_loss = criterion(real_validity, Variable(torch.ones(batch_size)).cuda())
+
+    # train with fake images
+    z = Variable(torch.randn(batch_size, 100)).cuda()
+    fake_labels = Variable(torch.LongTensor(np.random.randint(0, 10, batch_size))).cuda()
+    fake_images = generator(z, fake_labels)
+    fake_validity = discriminator(fake_images, fake_labels)
+    fake_loss = criterion(fake_validity, Variable(torch.zeros(batch_size)).cuda())
+
+    d_loss = real_loss + fake_loss
+    d_loss.backward()
+    d_optimizer.step()
+    return d_loss.item()
+
+
+d_optimizer = torch.optim.Adam(discriminator.parameters(), lr=1e-4)
+g_optimizer = torch.optim.Adam(generator.parameters(), lr=1e-4)
+
+for epoch in range(num_epochs):
+    print('Starting epoch {}...'.format(epoch))
+    for i, (images, labels) in enumerate(tqdm(data_loader)):
+        real_images = Variable(images).cuda()
+        labels = Variable(labels).cuda()
+        generator.train()
+        batch_size = real_images.size(0)
+        d_loss = discriminator_train_step(len(real_images), discriminator,
+                                          generator, d_optimizer, criterion,
+                                          real_images, labels)
+
+        g_loss = generator_train_step(batch_size, discriminator, generator, g_optimizer, criterion)
+
+    generator.eval()
+    print('g_loss: {}, d_loss: {}'.format(g_loss, d_loss))
+    z = Variable(torch.randn(9, 100)).cuda()
+    labels = Variable(torch.LongTensor(np.arange(9))).cuda()
+    sample_images = generator(z, labels).unsqueeze(1).data.cpu()
+    grid = make_grid(sample_images, nrow=3, normalize=True).permute(1, 2, 0).numpy()
+    plt.imshow(grid)
+    plt.show()
+
+z = Variable(torch.randn(100, 100)).cuda()
+labels = Variable(torch.LongTensor([i for _ in range(10) for i in range(10)])).cuda()
+sample_images = generator(z, labels).unsqueeze(1).data.cpu()
+grid = make_grid(sample_images, nrow=10, normalize=True).permute(1, 2, 0).numpy()
+fig, ax = plt.subplots(figsize=(15, 15))
+ax.imshow(grid)
+_ = plt.yticks([])
+_ = plt.xticks(np.arange(15, 300, 30),
+               ['T-Shirt', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'],
+               rotation=45, fontsize=20)
+plt.show()
