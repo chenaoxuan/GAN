f000cda55b9a7ff3e104e475b40fd55a0d8e1ac7
chenaoxuan <cax1165@163.com>
2022-07-03 11:27:17 +0800

    完成trainer

--- GAN/engine/trainer.py
+++ GAN/engine/trainer.py
@@ -12,33 +12,42 @@ import matplotlib.pyplot as plt
 from torchvision.utils import make_grid
 
 
-def generator_train_step(config, batch_size, discriminator, generator, g_optimizer, criterion):
+def generator_train_step(config, discriminator, generator, g_optimizer, criterion):
+    """
+    generate fake images and train generator by cheating discriminator.
+    """
     device = config.MODEL.DEVICE
+    batch_size = config.DATALOADER.BATCH_SIZE
     g_optimizer.zero_grad()
     z = Variable(torch.randn(batch_size, 100)).to(device)
     fake_labels = Variable(torch.LongTensor(np.random.randint(0, 10, batch_size))).to(device)
     fake_images = generator(z, fake_labels)
     validity = discriminator(fake_images, fake_labels)
+    # cheat discriminator
     g_loss = criterion(validity, Variable(torch.ones(batch_size)).to(device))
     g_loss.backward()
     g_optimizer.step()
     return g_loss.item()
 
 
-def discriminator_train_step(config, batch_size, discriminator, generator, d_optimizer, criterion, real_images, labels):
+def discriminator_train_step(config, discriminator, generator, d_optimizer, criterion, real_images, labels):
+    """
+    use real images and generated fake images to train discriminator
+    """
     device = config.MODEL.DEVICE
+    batch_size = len(real_images)
     d_optimizer.zero_grad()
 
     # train with real images
-    real_validity = discriminator(real_images, labels)
-    real_loss = criterion(real_validity, Variable(torch.ones(batch_size)).to(device))
+    real_val = discriminator(real_images, labels)
+    real_loss = criterion(real_val, Variable(torch.ones(batch_size)).to(device))
 
     # train with fake images
     z = Variable(torch.randn(batch_size, 100)).to(device)
     fake_labels = Variable(torch.LongTensor(np.random.randint(0, 10, batch_size))).to(device)
-    fake_images = generator(z, fake_labels)
-    fake_validity = discriminator(fake_images, fake_labels)
-    fake_loss = criterion(fake_validity, Variable(torch.zeros(batch_size)).to(device))
+    fake_imgs = generator(z, fake_labels)
+    fake_val = discriminator(fake_imgs, fake_labels)
+    fake_loss = criterion(fake_val, Variable(torch.zeros(batch_size)).to(device))
 
     d_loss = real_loss + fake_loss
     d_loss.backward()
@@ -50,29 +59,43 @@ def do_train(config, dataloader, generator, g_optim, discriminator, d_optim, cri
     device = config.MODEL.DEVICE
     for epoch in range(config.SOLVER.MAX_EPOCH):
         print(f'Starting epoch {epoch}')
-        for i, (images, labels) in enumerate(dataloader):
-            real_images = Variable(images).to(device)
+        for images, labels in dataloader:
+            real_imgs = Variable(images).to(device)
             labels = Variable(labels).to(device)
             generator.train()
-            batch_size = real_images.size(0)
-            d_loss = discriminator_train_step(config, len(real_images), discriminator,
-                                              generator, d_optim, criterion,
-                                              real_images, labels)
-
-            g_loss = generator_train_step(config, batch_size, discriminator, generator, g_optim, criterion)
+            d_loss = discriminator_train_step(config,
+                                              discriminator,
+                                              generator,
+                                              d_optim,
+                                              criterion,
+                                              real_imgs,
+                                              labels)
+            g_loss = generator_train_step(config,
+                                          discriminator,
+                                          generator,
+                                          g_optim,
+                                          criterion)
 
         generator.eval()
         print('g_loss: {}, d_loss: {}'.format(g_loss, d_loss))
         z = Variable(torch.randn(9, 100)).to(device)
         labels = Variable(torch.LongTensor(np.arange(9))).to(device)
-        sample_images = generator(z, labels).unsqueeze(1).data.cpu()
-        grid = make_grid(sample_images, nrow=3, normalize=True).permute(1, 2, 0).numpy()
-        plt.imshow(grid)
-        plt.show()
+        sample_imgs = generator(z, labels).unsqueeze(1).data.cpu()
+        step_show(sample_imgs)
     z = Variable(torch.randn(100, 100)).to(device)
     labels = Variable(torch.LongTensor([i for _ in range(10) for i in range(10)])).to(device)
-    sample_images = generator(z, labels).unsqueeze(1).data.cpu()
-    grid = make_grid(sample_images, nrow=10, normalize=True).permute(1, 2, 0).numpy()
+    sample_imgs = generator(z, labels).unsqueeze(1).data.cpu()
+    final_show(sample_imgs)
+
+
+def step_show(imgs):
+    grid = make_grid(imgs, nrow=3, normalize=True).permute(1, 2, 0).numpy()
+    plt.imshow(grid)
+    plt.show()
+
+
+def final_show(imgs):
+    grid = make_grid(imgs, nrow=10, normalize=True).permute(1, 2, 0).numpy()
     fig, ax = plt.subplots(figsize=(15, 15))
     ax.imshow(grid)
     _ = plt.yticks([])
